daa09786d87afdb2886b9b37842fc6b09bd62403
Jonathan C <jsea@uw.edu>
2019-08-30 02:46:20 -0700

    Added more design features including hints and rewards for guessing correctly. Added some CSS3 features

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,19 @@
-// GAME SETTINGS
+// START: Hide all of this code from the actual global scope
+(() => {
+
+// Game settings
 var MAX_GUESSES = 10;
-var HANDLE_REPEAT_GUESSES = true;
+var HANDLE_REPEAT_GUESSES = false;
 var WORD_LIST = [
-    'watermelon',
-    'pineapple',
-    'orange',
-    'banana',
-    'apple',
-    'pear'
+    {w:'Steven Universe', h:'<h3>T.V. Show (Animated)</h3><p>This show sets itself apart with incredibly heavy-hitting morals as it progresses. If you haven\'t seen this show, I highly <em>highly</em> recommend it for all ages. It\'s a show full of love without stipulations.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/MIREK5ZL1jA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+    {w:'Adventure Time', h:'<h3>T.V. Show (Animated)</h3><p>Amusing and energetic, this show sets itself apart with its wildly weird world based on Celtic mythology and its deep exploration of friendship and overcoming life\'s hardships.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/gfijG7pmMqk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+    {w:'Galavant', h:'<h3>T.V. Show (Musical)</h3><p>Hilariously tongue-in-cheek, this show is a great way to relax and enjoy your leisure time. Follow our protagonist as they journey to save their beloved. And yes, they all really do sing. It\'s amazing.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QWnDwM0RSX4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+    {w:'Dead Cells', h:'<h3>Roguelike Video Game</h3><p>One of the best aesthetics in a roguelike in recent years, this game continues to challenge you more and more even after you manage to beat it\'s increasingly-harder modes. While a solid roguelike, it is also a top-quality metroidvania game.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/RvGaSPTcTxc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+    {w:'Aggretsuko', h:'<h3>T.V. Show (Animated)</h3><p>One of the most frustratingly-relatable and loveable shows, navigating feelings plays a key role in its protagonist\'s daily life just trying to survive, and we get to share in the ups and downs that come along with them.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/z9jGaJJlNyo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+    {w:'Metric', h:'<h3>Music Band (Indie Rock)</h3><p>The female vocal lead of this quartet carries a diverse array of songs across over six albums varying greatly in genre and feel. Great to sing and dance along with.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/uly3S2KjUf4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+    {w:'Muse', h:'<h3>Music Band (Rock)</h3><p>This band never shies away from culturally-relevant lyrics. The male vocal lead carries several albums across multiple different genres. You can try to sing along with him, but it will be difficult without a wide vocal range.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/X8f5RgwY8CI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+    {w:'Linkin Park', h:'<h3>Music Band (Rock)</h3><p>This band arrived in the 90s with a powerful dynamic, self-written lyrics, and an outlet for all the frustrations of life. Love them or hate them, they were always working on expanding their musical range and accomplished multiple unique albums with an artistic theme behind each one. You\'ll run out of breath singing along.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/AsNvb56CTa4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+    {w:'K.Flay', h:'<h3>Music Artist (Rock)</h3><p>This artist came onto the scene in the 2000s while double-majoring at Stanford but really only hit it big in 2017 with an album that is a reflection of life\'s many intense ups and downs.</p>',r:'<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/3SHFLj-pTQE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
 ];
 var VALID_LETTER_LIST = [
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
@@ -20,7 +26,7 @@ var game = {
     // Set up variables we'll be using to keep track of game data
     currentState: '',
     nextState: '',
-    currentWord: '',
+    currentWordIndex: -1,
     uniqueLetters: 0,
     wins: 0,
     losses: 0,
@@ -33,25 +39,32 @@ var game = {
         console.log('setting up new game');
 
         // Get a new word while ensuring it isn't the same word as the last one played by the user
-        var newWord = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)].toLowerCase();
-        while (newWord === this.currentWord) {
-            newWord = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)].toLowerCase();
+        var newWordIndex = Math.floor(Math.random() * WORD_LIST.length);
+        while (newWordIndex === this.currentWordIndex) {
+            newWordIndex = Math.floor(Math.random() * WORD_LIST.length);
         }
-        this.currentWord = newWord;
+        this.currentWordIndex = newWordIndex;
 
         // Determine the amount of unique characters utilizing a unique-set data structure to our advantage
-        this.uniqueLetters = (new Set(newWord)).size;
+        var newWord = WORD_LIST[newWordIndex].w.toLocaleLowerCase();
+        var targetNonAlphabeticCharsRegEx = /[^a-z]/g;
+        var newWordAlphabeticCharsOnly = newWord.replace(targetNonAlphabeticCharsRegEx, '');
+        this.uniqueLetters = (new Set(newWordAlphabeticCharsOnly)).size;
 
         // Reset the game state
         this.guesses = 0;
         this.keysRevealed = [];
         this.keysIncorrect = [];
 
+        document.getElementById('hint').innerHTML = WORD_LIST[newWordIndex].h;
+
         console.log(`New word: ${newWord} with ${this.uniqueLetters} unique letters`);
     },
 
     // Create a method for applying guessing logic to a given key character
     makeGuess: function(keyGuessed) {
+        keyGuessed = keyGuessed.toLocaleLowerCase();
+
         // If our key has been guessed before, we waste a guess (if enabled)
         if (this.keysIncorrect.indexOf(keyGuessed) !== -1 || this.keysRevealed.indexOf(keyGuessed) !== -1) {
             if (HANDLE_REPEAT_GUESSES) {
@@ -59,7 +72,7 @@ var game = {
             }
         }
         // If our key doesn't exist in the current word, we waste a guess
-        else if (this.currentWord.indexOf(keyGuessed) === -1) {
+        else if (WORD_LIST[this.currentWordIndex].w.toLocaleLowerCase().indexOf(keyGuessed) === -1) {
             this.handleIncorrectGuess(keyGuessed);
         }
         // Otherwise, we are a newly-guessed key and should reveal the relevant letters
@@ -138,13 +151,15 @@ var game = {
         }
 
         var numberOfLettersElements = document.getElementsByClassName('number-of-letters');
+        var targetNonAlphabeticCharsRegEx = /[^a-z]/g;
+        var currentWordAlphabeticCharsOnly = WORD_LIST[this.currentWordIndex].w.toLocaleLowerCase();
         for (var i = 0; i < numberOfLettersElements.length; i++) {
-            numberOfLettersElements[i].textContent = this.currentWord.length;
+            numberOfLettersElements[i].textContent = currentWordAlphabeticCharsOnly.replace(targetNonAlphabeticCharsRegEx, '').length;
         }
 
         var currentWordElements = document.getElementsByClassName('current-word');
         for (var i = 0; i < currentWordElements.length; i++) {
-            currentWordElements[i].textContent = this.currentWord;
+            currentWordElements[i].textContent = WORD_LIST[this.currentWordIndex].w;
         }
 
         var winsPluralizerElements = document.getElementsByClassName('wins-pluralizer');
@@ -169,15 +184,15 @@ var game = {
 
         var correctLettersGuessed = document.getElementById('correct-letters-guessed');
         var revealedLettersString = '';
-        for (var i = 0; i < this.currentWord.length; i++) {
+        for (var i = 0; i < WORD_LIST[this.currentWordIndex].w.length; i++) {
 
-            if (i !== 0) {
-                revealedLettersString += ' ';
+            if (this.keysRevealed.indexOf(WORD_LIST[this.currentWordIndex].w[i].toLocaleLowerCase()) !== -1) {
+
+                revealedLettersString += WORD_LIST[this.currentWordIndex].w[i];
             }
-            
-            if (this.keysRevealed.indexOf(this.currentWord[i]) !== -1) {
-                
-                revealedLettersString += this.currentWord[i];
+            else if (VALID_LETTER_LIST.indexOf(WORD_LIST[this.currentWordIndex].w[i].toLocaleLowerCase()) === -1) {
+
+                revealedLettersString += WORD_LIST[this.currentWordIndex].w[i];
             }
             else {
 
@@ -188,6 +203,9 @@ var game = {
 
         var incorrectLettersGuessed = document.getElementById('incorrect-letters-guessed');
         var incorrectLettersString = this.keysIncorrect.join(", ");
+        if (incorrectLettersString === '') {
+            incorrectLettersString = '(none)';
+        }
         incorrectLettersGuessed.textContent = incorrectLettersString;
     },
 
@@ -208,16 +226,16 @@ var game = {
 
                 // NOTE: Since 'this' isn't referencing the game object in here, we'll just reference it through the global 'game' variable
 
+                // Set up a new game
+                game.setupNewGame();
+
                 // Update the new game state's screen
                 game.updateScreen();
 
                 document.onkeyup = function(event) {
 
                     // If any key is pressed, we should start the game
-                    
-                    // Set up a new game
-                    game.setupNewGame();
-                    
+
                     // Start waiting for letter guessing by the player
                     game.switchState('wait-for-game-input');
                 };
@@ -249,13 +267,13 @@ var game = {
                 document.onkeyup = function(event) {
 
                     // If an acceptable letter key is pressed, get it handled
-                    if (VALID_LETTER_LIST.indexOf(event.key.toLowerCase()) !== -1) {
+                    if (VALID_LETTER_LIST.indexOf(event.key.toLocaleLowerCase()) !== -1) {
 
                         // Make the guess with the user's key
-                        game.makeGuess(event.key.toLowerCase());
+                        game.makeGuess(event.key.toLocaleLowerCase());
                     }
                     // If we don't want to play this word anymore, we can hit the 'escape' key to return to the start screen
-                    else if (event.key.toLowerCase() === 'escape') {
+                    else if (event.key.toLocaleLowerCase() === 'escape') {
 
                         game.losses++;
                         
@@ -286,7 +304,7 @@ var game = {
                 document.onkeyup = function(event) {
 
                     // If we don't want to play this word anymore, we can hit the 'escape' key to return to the start screen
-                    if (event.key.toLowerCase() === 'escape') {
+                    if (event.key.toLocaleLowerCase() === 'escape') {
 
                         game.losses++;
                         
@@ -305,6 +323,9 @@ var game = {
 
                 // Hide the lose pop-up
                 document.getElementById('game-lose-popup').setAttribute('class', 'display-none');
+
+                // Hide the pop-up overlay
+                document.getElementById('game-popup-overlay').setAttribute('class', 'display-none');
 
                 // Hide the game screen if we're going back to the start screen
                 if (game.nextState === 'start-screen') {
@@ -325,7 +346,7 @@ var game = {
                 document.onkeyup = function(event) {
 
                     // If the 'enter' key is pressed
-                    if (event.key.toLowerCase() === 'enter' || event.key.toLowerCase() === 'return') {
+                    if (event.key.toLocaleLowerCase() === 'enter' || event.key.toLocaleLowerCase() === 'return') {
 
                         // We'll restart the game with a new word
                         game.setupNewGame();
@@ -334,6 +355,9 @@ var game = {
                         game.switchState('wait-for-game-input');
                     }
                 }
+
+                // Show the pop-up overlay
+                document.getElementById('game-popup-overlay').setAttribute('class', 'display-block');
 
                 // Show the lose pop-up
                 document.getElementById('game-lose-popup').setAttribute('class', 'display-block');
@@ -346,8 +370,14 @@ var game = {
                 // Make sure our key event handler is removed
                 document.onkeyup = undefined;
 
+                // Clear the reward box
+                document.getElementById('reward').innerHTML = '';
+
                 // Hide the win pop-up
                 document.getElementById('game-win-popup').setAttribute('class', 'display-none');
+
+                // Hide the pop-up overlay
+                document.getElementById('game-popup-overlay').setAttribute('class', 'display-none');
 
                 // Hide the game screen if we're going back to the start screen
                 if (game.nextState === 'start-screen') {
@@ -368,7 +398,7 @@ var game = {
                 document.onkeyup = function(event) {
 
                     // If the 'enter' key is pressed
-                    if (event.key.toLowerCase() === 'enter' || event.key.toLowerCase() === 'return') {
+                    if (event.key.toLocaleLowerCase() === 'enter' || event.key.toLocaleLowerCase() === 'return') {
 
                         // We'll restart the game with a new word
                         game.setupNewGame();
@@ -377,6 +407,12 @@ var game = {
                         game.switchState('wait-for-game-input');
                     }
                 }
+
+                // Fill the reward box with the provided reward HTML
+                document.getElementById('reward').innerHTML = WORD_LIST[game.currentWordIndex].r;
+
+                // Show the pop-up overlay
+                document.getElementById('game-popup-overlay').setAttribute('class', 'display-block');
 
                 // Show the win pop-up
                 document.getElementById('game-win-popup').setAttribute('class', 'display-block');
@@ -412,3 +448,6 @@ var game = {
 
 // Load the game
 game.switchState('start-screen');
+
+// END: Hide all of this code from the actual global scope
+})();
