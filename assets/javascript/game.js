5e3e19620d72a15cc57e1ba62b18834ec05b57b9
Jonathan C <jsea@uw.edu>
2019-08-29 21:40:07 -0700

    Hooked up UI to the game logic. Fixed bug where guesses left were decreasing on correct guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -104,18 +104,11 @@ var game = {
     handleCorrectGuess(keyGuessed) {
         console.log(`correctly guessing '${keyGuessed}'`);
 
-        this.guesses++;
-
         this.keysRevealed.push(keyGuessed);
 
         // If we guessed all the characters correctly, we won the game.
-        // NOTE: We do this check before determining if max guesses have been reached because we want the player to be able to win on their last guess.
         if (this.uniqueLetters === this.keysRevealed.length) {
             this.switchState('game-win');
-        }
-        // If we've reached maximum guesses, we lost the game
-        else if (this.guesses === MAX_GUESSES) {
-            this.switchState('game-lose');
         }
         // Otherwise, continue on as usual and just update the screen
         else {
@@ -126,6 +119,51 @@ var game = {
     // Create a method for updating the screen
     updateScreen() {
         console.log("screen updating");
+
+        var numberOfWinsElements = document.getElementsByClassName('number-of-wins');
+        for (var i = 0; i < numberOfWinsElements.length; i++) {
+            numberOfWinsElements[i].textContent = this.wins;
+        }
+
+        var numberOfLossesElements = document.getElementsByClassName('number-of-losses');
+        for (var i = 0; i < numberOfLossesElements.length; i++) {
+            numberOfLossesElements[i].textContent = this.losses;
+        }
+
+        var numberOfGuessesLeftElements = document.getElementsByClassName('number-of-guesses-left');
+        for (var i = 0; i < numberOfGuessesLeftElements.length; i++) {
+            numberOfGuessesLeftElements[i].textContent = MAX_GUESSES - this.guesses;
+        }
+
+        var numberOfLettersElements = document.getElementsByClassName('number-of-letters');
+        for (var i = 0; i < numberOfLettersElements.length; i++) {
+            numberOfLettersElements[i].textContent = this.currentWord.length;
+        }
+
+        var currentWordElements = document.getElementsByClassName('current-word');
+        for (var i = 0; i < currentWordElements.length; i++) {
+            currentWordElements[i].textContent = this.currentWord;
+        }
+
+        var winsPluralizerElements = document.getElementsByClassName('wins-pluralizer');
+        for (var i = 0; i < winsPluralizerElements.length; i++) {
+            if (this.wins !== 1) {
+                winsPluralizerElements[i].setAttribute("class", "wins-pluralizer");
+            }
+            else {
+                winsPluralizerElements[i].setAttribute("class", "wins-pluralizer display-none");
+            }
+        }
+
+        var lossesPluralizerElements = document.getElementsByClassName('losses-pluralizer');
+        for (var i = 0; i < lossesPluralizerElements.length; i++) {
+            if (this.losses !== 1) {
+                lossesPluralizerElements[i].setAttribute("class", "losses-pluralizer");
+            }
+            else {
+                lossesPluralizerElements[i].setAttribute("class", "losses-pluralizer display-none");
+            }
+        }
     },
 
     // We'll use a state machine to keep track of our different game states' logic
@@ -140,10 +178,14 @@ var game = {
             loadState: function() {
                 console.log("loading: start-screen");
 
+                // NOTE: Since 'this' isn't referencing the game object in here, we'll just reference it through the global 'game' variable
+
+                // Update the new game state's screen
+                game.updateScreen();
+
                 document.onkeyup = function(event) {
 
                     // If any key is pressed, we should start the game
-                    // NOTE: Since 'this' isn't referencing the game object in here, we'll just reference it through the global 'game' variable
                     
                     // Set up a new game
                     game.setupNewGame();
@@ -163,9 +205,12 @@ var game = {
             loadState: function() {
                 console.log("loading: waiting for game input");
 
-                document.onkeyup = function(event) {
+                // NOTE: Since 'this' isn't referencing the game object in here, we'll just reference it through the global 'game' variable
 
-                    // NOTE: Since 'this' isn't referencing the game object in here, we'll just reference it through the global 'game' variable
+                // Update the new game state's screen
+                game.updateScreen();
+
+                document.onkeyup = function(event) {
 
                     // If an acceptable letter key is pressed, get it handled
                     if (VALID_LETTER_LIST.indexOf(event.key.toLowerCase()) !== -1) {
@@ -204,17 +249,23 @@ var game = {
                 
                 // NOTE: Since 'this' isn't referencing the game object in here, we'll just reference it through the global 'game' variable
 
+                // Update the game state
                 game.losses++;
 
+                // Update the new game state's screen
                 game.updateScreen();
 
                 document.onkeyup = function(event) {
-                    
-                    // If the 'enter' key is pressed, we'll restart the game with a new word
-                    game.setupNewGame();
 
-                    // Go straight back into the game skipping the start screen
-                    game.switchState('wait-for-game-input');
+                    // If the 'enter' key is pressed
+                    if (event.key.toLowerCase() === 'enter' || event.key.toLowerCase() === 'return') {
+
+                        // We'll restart the game with a new word
+                        game.setupNewGame();
+
+                        // Go straight back into the game skipping the start screen
+                        game.switchState('wait-for-game-input');
+                    }
                 }
             }
         },
@@ -230,17 +281,23 @@ var game = {
 
                 // NOTE: Since 'this' isn't referencing the game object in here, we'll just reference it through the global 'game' variable
 
+                // Update the game state
                 game.wins++;
 
+                // Update the new game state's screen
                 game.updateScreen();
 
                 document.onkeyup = function(event) {
-                    
-                    // If the 'enter' key is pressed, we'll restart the game with a new word
-                    game.setupNewGame();
 
-                    // Go straight back into the game skipping the start screen
-                    game.switchState('wait-for-game-input');
+                    // If the 'enter' key is pressed
+                    if (event.key.toLowerCase() === 'enter' || event.key.toLowerCase() === 'return') {
+
+                        // We'll restart the game with a new word
+                        game.setupNewGame();
+    
+                        // Go straight back into the game skipping the start screen
+                        game.switchState('wait-for-game-input');
+                    }
                 }
             }
         },
